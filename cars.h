913b30294800fa2c063baa92cc81a464cebe7e02
Haroliao <83564380+Haroliao@users.noreply.github.com>
2021-10-13 01:08:11 +1030

    Update cars.h

--- cars.h
+++ cars.h
@@ -20,8 +20,6 @@ public:
 	int tire_in_use;
 
 	//functions
-	float get_tire_level();
-	float get_fuel_level();
 	int get_tire();
 	void print_tire_in_use();
 	void change_tires(int tire_select);//function to change tires of car
@@ -35,20 +33,16 @@ class ferrari:public cars{
 public:
     float fuel_level;
 	float tire_level;//stores the tire choice as an int value, to access elements in tire_available string array
+	float get_tire_level();
+	float get_fuel_level();
 	double tire_deg(int lap_count);//reduces tire level using linear regression
 	double fuel_deg(int lap_count);//reduces fuel level using linear regression
 };
-class toyota:public cars{
-public:
-    float fuel_level;
-	float tire_level;//stores the tire choice as an int value, to access elements in tire_available string array
-	double tire_deg(int lap_count);//reduces tire level using linear regression
-	double fuel_deg(int lap_count);//reduces fuel level using linear regression
+class toyota:public cars, public ferrari{
+
+
 };
-class mercedes:public cars{
-public:
-    float fuel_level;
-	float tire_level;//stores the tire choice as an int value, to access elements in tire_available string array
-	double tire_deg(int lap_count);//reduces tire level using linear regression
-	double fuel_deg(int lap_count);//reduces fuel level using linear regression
+
+class mercedes:public cars, public ferrari{
+
 };
